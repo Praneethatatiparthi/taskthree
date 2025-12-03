0ce4461f36dad0ea08211645f5741551bd2fa9c4
Praneethatatiparthi <2500030074@kluniversity.in>
2025-12-03 08:52:42 +0530

    first commit

---
+++ task3.js
@@ -0,0 +1,19 @@
+function convertNumber(){
+const num = parseInt(document.getElementById("input1").value);
+const format = document.getElementById("formatSelect").value;
+const resultDiv = document.getElementById("result");
+if(isNaN(num)){
+resultDiv.innerText = "Please enter a valid number.";
+resultDiv.style.color = "red";
+return;    
+}
+let result = "";
+if(format === "octal"){
+result = num.toString(8);    
+}
+else if(format === "hexadecimal"){
+result = num.toString(16).toUpperCase(); 
+}
+resultDiv.style.color = "green";
+resultDiv.innerText = `ConvertedValue:${result}`;
+}
